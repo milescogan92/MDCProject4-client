a5908e8bd95c78c904141f5e6e798a5cf08806c0
milescogan92 <miles.cogan92@gmail.com>
2020-10-28 11:03:50 -0400

    Coded scripts/repairs/events.js file
    coded out the events.js file, made a couple of pseudo code notes regarding possible changes if I run into bugs.

---
+++ assets/scripts/repairs/events.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const getFormFields = require('../../../lib/get-form-fields.js')
+const api = require('./api.js')
+const ui = require('./ui.js')
+const store = require('../store.js')
+
+const onIndexRepairs = function (event) {
+  event.preventDefault()
+  api.indexRepairs()
+    .then(ui.onIndexRepairsSuccess)
+    .catch(ui.onIndexRepairsFailure)
+}
+
+const onShowRepair = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const data = getFormFields(form)
+  const repairId = data.repair.id // maybe plural, data.repairs.id?
+  api.showRepair(repairId)
+    .then(ui.onShowRepairSuccess)
+    .catch(ui.onShowRepairFailure)
+}
+
+const onCreateRepair = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const data = getFormFields(form)
+  api.createRepair(data)
+    .then(ui.onCreateRepairSuccess)
+    .catch(ui.onCreateRepairFailure)
+}
+
+const onUpdateRepair = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const data = getFormFields(form)
+  api.updateRepair(data)
+    .then(ui.onUpdateRepairSuccess)
+    .catch(ui.onUpdateRepairFailure)
+}
+
+const onDeleteRepair = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const data = getFormFields(form)
+  const repairId = data.repair.id // maybe plural, data.repairs.id?
+  api.deleteRepair(repairId)
+    .then(ui.onDeleteRepairSuccess)
+    .catch(ui.onDeleteRepairFailure)
+}
+
+module.exports = {
+  onIndexRepairs: onIndexRepairs,
+  onShowRepair: onShowRepair,
+  onCreateRepair: onCreateRepair,
+  onUpdateRepair: onUpdateRepair,
+  onDeleteRepair: onDeleteRepair
+}
