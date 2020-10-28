a527a8be43834bf1163657d51bf54d79885566f2
milescogan92 <miles.cogan92@gmail.com>
2020-10-28 09:55:06 -0400

    Edited app.js file, created assets/scripts/auth and assets/scripts/repairs
    Coded the app.js file to communicate with the index.html file
    
    created new scripts folders for auth and repairs
    
    created new files in each folder: api.js, ui.js, events.js - blank right now.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,5 +7,25 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  const authEvents = require('./auth/events.js')
+  const repairEvents = require('./repairs/events.js')
+
+  $(() => {
+    // $('#sign-out-button').hide()
+    // $('#change-password-form').hide()
+    // $('#index-repairs-button').hide()
+    // $('#create-repair-form').hide()
+    // $('#update-repair-form').hide()
+    // $('#delete-repair-form').hide()
+    // $('#show-repair-form').hide()
+    $('#sign-up-form').on('submit', authEvents.onSignUp)
+    $('#sign-in-form').on('submit', authEvents.onSignIn)
+    $('#change-password-form').on('submit', authEvents.onChangePassword)
+    $('#sign-out-button').on('click', authEvents.onSignOut)
+    $('#index-repairs-button').on('click', repairEvents.onIndexRepairs)
+    $('#create-repair-form').on('submit', repairEvents.onCreateRepair)
+    $('#update-repair-form').on('submit', repairEvents.onUpdateRepair)
+    $('#delete-repair-form').on('submit', repairEvents.onDeleteRepair)
+    $('#show-repair-form').on('submit'), repairEvents.onShowRepair)
+  })
 })
