095419f0662143e07dfd9ebd8dcd9c9af82edb22
milescogan92 <miles.cogan92@gmail.com>
2020-11-02 08:22:13 -0500

    Added new font, edited config.js again
    added new font by importing to HTML page and using on CSS stylesheet
    
    removed the last backslash from the heroku deployed address in the config.js file
    
    still having deploy issues on front end.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://arcane-lake-50388.herokuapp.com/',
+  production: 'https://arcane-lake-50388.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
